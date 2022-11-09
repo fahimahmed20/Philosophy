4d9fd6e692a6b28574e96d748c6a56a0ebd53a65
Fahim Ahmed <this.fahimahmed@gmail.com>
2022-11-09 11:26:14 +0000

    update

--- index.php
+++ index.php
@@ -12,22 +12,6 @@
     ================================================== -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    <!-- CSS
-    ================================================== -->
-    <!-- <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/vendor.css">
-    <link rel="stylesheet" href="css/main.css"> -->
-
-    <!-- script
-    ================================================== -->
-    <!-- <script src="js/modernizr.js"></script>
-    <script src="js/pace.min.js"></script> -->
-
-    <!-- favicons
-    ================================================== -->
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
-
     <?php wp_head();?>
 </head>
 
