81185ffa1fe606bea69a76ee7a86d690ee09d8f9
fahimahmed20 <this.fahimahmed@gmail.com>
2022-12-09 02:51:37 +0600

    single page

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 
     <!-- pageheader
     ================================================== -->
-    <section class="s-pageheader s-pageheader--home">
+    <section class="s-pageheader <?php if(is_home()){ echo 's-pageheader--home';}?>">
 
         <header class="header">
             <div class="header__content row">
@@ -83,7 +83,12 @@
 
             </div> <!-- header-content -->
         </header> <!-- header -->
+        
+        <?php if(is_home()) {
+            get_template_part('template-parts/featured');
+        }  else{
 
-        <?php get_template_part('template-parts/featured'); ?>
+        }
+        ?>
 
     </section> <!-- end s-pageheader -->
